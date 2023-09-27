fb64d9230311367927b53cb50213c6381f35b003
TuanTu237 <tuvieclam247@gmail.com>
2023-09-27 21:45:15 +0700

    up

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
 public class main {
-    
+    static CompanyManagement companyManagement = new CompanyManagement("2023 COMPANY");
+    static int insuranceID =0;
+    public static void main(String[] args){
+
+    }
 }
